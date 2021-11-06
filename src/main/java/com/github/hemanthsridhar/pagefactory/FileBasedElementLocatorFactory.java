35f1790e11529efc9f6b415e2511711877dab53c
Hemanth Sridhar <hemanthsridhar@Hemanths-MacBook-Pro.local>
2021-11-06 14:13:17 +0530

    Added annotation for looking up filePath

---
+++ src/main/java/com/github/hemanthsridhar/pagefactory/FileBasedElementLocatorFactory.java
@@ -0,0 +1,27 @@
+package com.github.hemanthsridhar.pagefactory;
+
+import com.github.hemanthsridhar.builder.CustomAnnotations;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.support.pagefactory.ElementLocator;
+import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by hemanthsridhar on 1/6/19.
+ */
+
+public class FileBasedElementLocatorFactory implements ElementLocatorFactory {
+    private final SearchContext searchContext;
+    private final Object page;
+
+    public FileBasedElementLocatorFactory(SearchContext searchContext, Object page) {
+        this.searchContext = searchContext;
+        this.page = page;
+    }
+
+    @Override
+    public ElementLocator createLocator(Field field) {
+        return new FileBasedElementLocator(searchContext, new CustomAnnotations(field, page));
+    }
+}
